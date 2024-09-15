ca52593d80d096c1bbffd47056d3c467adc44985
Jason Melgoza <jasonmelgoza@gmail.com>
2024-09-15 11:42:43 -0700

    Remove unused files and scripts

--- _site/js/list.js
+++
@@ -1,9 +0,0 @@
-//
-// List.js
-// For adding search, sort, filters, and flexibility to tables, lists and various HTML elements.
-// https://github.com/javve/list.js/
-
-var options = {
-  valueNames: [ 'name' ]
-};
-var iconList = new List('icons', options);
